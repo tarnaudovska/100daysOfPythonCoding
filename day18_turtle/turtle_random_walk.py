92e13fe72329d69d23d383b0102259b6321ec862
Tamara <tdanevska@gmail.com>
2024-08-21 16:22:13 +0200

    randomizing color with function

--- day18_turtle/turtle_random_walk.py
+++ day18_turtle/turtle_random_walk.py
@@ -1,20 +1,25 @@
 #####Turtle Square######
 
 import turtle as t
-from colors_options import color_options
 import random
 
 tim = t.Turtle()
 tim.shape("turtle")
-tim.color("yellow")
 t.bgcolor("lavender")
 tim.pensize(10)
 tim.speed("fastest")
 
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+    
 directions = [0, 90, 180, 270]
 
 for _ in range(100):
-    tim.color(random.choice(color_options))
+    tim.color(random_color())
     tim.forward(50)
     tim.setheading(random.choice(directions))
     
