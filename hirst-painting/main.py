cd05072c7391a6e389222341ea30c8bfcc734fe3
Tamara <tdanevska@gmail.com>
2024-08-27 15:35:48 +0200

    Creating painting with dots, using turtle

---
+++ hirst-painting/main.py
@@ -0,0 +1,34 @@
+# Creating a list of colors from an image
+# import colorgram
+
+# colors = colorgram.extract('image.jpg', 30)
+# rgb_colors = []
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+import turtle as t
+import random
+
+t.colormode(255)
+colors = [(118, 166, 197), (126, 90, 58), (188, 242, 251), (121, 227, 251), (200, 156, 106), (143, 182, 155), (210, 212, 96), (61, 41, 21), (197, 234, 216), (59, 83, 9), (54, 27, 37), (154, 161, 57), (242, 228, 191), (128, 72, 80), (186, 147, 173), (159, 212, 184), (69, 108, 65), (20, 41, 12), (31, 39, 53), (99, 49, 38), (69, 105, 126), (185, 102, 90), (95, 46, 55), (164, 106, 125), (43, 83, 20), (91, 144, 157), (98, 155, 118), (224, 170, 188), (235, 199, 209), (115, 123, 163)]
+tim = t.Turtle()
+tim.screen.title('Painting with dots')
+tim.screen.setup (width=600, height=600, startx=450, starty=200)
+tim.hideturtle()
+tim.penup()
+tim.speed("fastest")
+
+for i in range(10):
+    tim.goto(-225, (50*i)-225)
+    for _ in range(10):
+        tim.dot(20, random.choice(colors))
+        tim.forward(50)
+    
+t.exitonclick()
