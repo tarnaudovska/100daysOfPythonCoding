d6727552f9147ddf924f8f0b2effef60c6e9d24b
Tamara Arnaudovska <tdanevska@gmail.com>
2024-12-24 17:35:04 +0100

    git rm Day 25 related files

--- Day_25_CSVs/main_CSV_example.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("./Day_25_CSVs/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
