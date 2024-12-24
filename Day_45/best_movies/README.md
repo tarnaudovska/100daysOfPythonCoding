36df3ec3e7b09c6010f728f693cfff8c6d8d02a4
Tamara Arnaudovska <tdanevska@gmail.com>
2024-12-24 18:39:47 +0100

    Day_45 name update and files update

---
+++ Day_45/best_movies/README.md
@@ -0,0 +1,13 @@
+# 🎥 Best Movies Scraper
+
+This Python script scrapes a list of the best movies from a given website and saves the titles into a `.txt` file.
+
+---
+
+## 🛠️ Features:
+- Uses `requests` to fetch data from a webpage.
+- Parses the HTML content with `BeautifulSoup` to extract movie titles.
+- Reverses the list to display the movies in ascending order.
+- Saves the cleaned movie titles into a text file (`greatest_shows.txt`).
+
+---
--- Day_45/best_movies/main.py
+++ Day_45/best_movies/main.py
@@ -12,7 +12,7 @@ all_movies = soup.find_all(name = "h3", class_="title")
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
 
-with open("./day_45/best_movies/movies.txt", mode="w", encoding="utf-8") as file:
+with open("./day_45/best_movies/greatest_shows.txt", mode="w", encoding="utf-8") as file:
     for movie in movies:
         clean_movie = movie.replace("12:", "12)")
         file.write(f"{clean_movie}\n")
